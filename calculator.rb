6656a1422256ec7bb499b2a874e75a7bddbbcab8
Mokhtar Ali <joemokhtarali@gmail.com>
2019-12-18 01:11:17 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 def calculator 
-  first_number = get_variable_from_file('./calculator.rb', "first_number")
-  second_number = get_variable_from_file('./calculator.rb', "second_number") && != 0
+  first_number = 2
+  second_number = 3
   sum = first_number + second_number
   difference = first_number - second_number
   product = first_number * second_number
